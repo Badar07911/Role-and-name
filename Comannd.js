1d82e993606ebcc1585d0ed27be392c600c286c9
Badar07911 <53259970+Badar07911@users.noreply.github.com>
2019-08-24 15:26:13 +0300

    Update Comannd.js

--- Comannd.js
+++ Comannd.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.login('NjE0NzgzOTAyNDA5Njg3MDUw.XWEgYg.Dc7MgF9PhVkaDtKCZkW1ZyZkn_g') 
+
+//--------------------------------------------------------------------------
+
 client.on(guildMember,Update',(oldMember,newmember)->
 {
 var role = newMember.guild.roles.find('name','BG');
