4844bd56a346b33bc64c8c1ec1f1c66051a5e76d
Badar07911 <53259970+Badar07911@users.noreply.github.com>
2019-08-27 03:34:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client
-
-client.login('NjE0NzgzOTAyNDA5Njg3MDUw.XWEgYg.Dc7MgF9PhVkaDtKCZkW1ZyZkn_g') 
-
+const client = new Discord.Client 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.login('NjE0NzgzOTAyNDA5Njg3MDUw.XWEgYg.Dc7MgF9PhVkaDtKCZkW1ZyZkn_g') 
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
+
+
 var prefix = "@! 『 𝐭𝖌 | 𝙱𝕒𝘥𝕒ᖇ 』#٩٥٢٦"
 
-```if(message.content.startsWith(prefix))
+if(message.content.startsWith(prefix))
          client.channels.find('id', '582093431770185745').send(` **فيه واحد يبيك**  `)
-);```
+) 
+});
+
+client.login('NjE0NzgzOTAyNDA5Njg3MDUw.XWEgYg.Dc7MgF9PhVkaDtKCZkW1ZyZkn_g')
